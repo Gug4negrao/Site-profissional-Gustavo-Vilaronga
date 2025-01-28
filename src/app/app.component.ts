5af138af43f3870411c8737633d1d2aa775d2427
Gustavo <gustavov@numax.com.br>
2025-01-28 17:34:49 -0300

    iniciando banco de traduções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Translations } from './constants/translations';
+import { TranslationService } from './services/translation.service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'PortiflioGustavo';
+export class AppComponent implements OnInit {
+  title: string = 'PortiflioGustavo';
+  language: string = 'pt-br';
+  constants: any = {};
+
+  constructor(private translationService: TranslationService){}
 
   RedirectSocialMedias(linkMedia : string){
     window.open(linkMedia, "_blank");
+  }
+
+  ngOnInit(): void {
+    this.language = this.translationService.getLanguage();
+    this.translationLoad();
+  }
+
+  translationLoad(): void{
+    this.constants = {
+      socialMedia: Translations.SocialMedia[this.language]
+    }
+  }
+
+  setLanguage(language: string): void{
+    this.translationService.setLanguage(language);
+    window.location.reload();
   }
 }
---
+++ src/app/constants/translations.ts
@@ -0,0 +1,7 @@
+export const Translations: any = {
+    SocialMedia: {
+        'pt-br': 'Redes Sociais',
+        'en-us': 'Social Medias',
+        'es-es': 'Redes Sociales'
+    }
+}
