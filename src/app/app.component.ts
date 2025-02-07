141faa582a44afd741285c91f2bf589bc8427398
Gustavo <gustavov@numax.com.br>
2025-02-07 15:59:45 -0300

    ajuste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
